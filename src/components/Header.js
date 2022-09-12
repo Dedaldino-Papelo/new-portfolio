258b8dff8f07dee352e2f9a7f2c74a9cef2fb738
Dedalino <kelsonpapelo18@gmail.com>
2022-09-13 00:23:54 +0100

    added logo and favicon to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import { Link } from 'react-scroll'
+import D from '../assets/D.png'
 
 const Header = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Dedaldino</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img src={D} alt='D' style={{width: '40px', height: '40px'}} />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
